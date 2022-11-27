8f9df5ef8205e769a8a219630901bc4615c32106
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-11-27 14:29:44 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -17,6 +17,8 @@ st.write('df.head()')
 if st.checkbox("Show number of rows and columns"):
   st.write(f'Rows: {df.shape[0]}')
   st.write(f'Columns: {df.shape[1]}')
+  
+st.dataframe(df.head())
 
 if st.checkbox('checkbox'):
     st.image("mike.png") 
