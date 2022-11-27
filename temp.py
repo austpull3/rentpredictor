a48dfaee6d77cd49b8e80c35e804b9fe88af0c3a
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-11-27 14:10:19 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -8,22 +8,14 @@ import numpy as np
 
 st.button('Streamlit Button', help = "Click me!")
 
-from sklearn.linear_model import LinearRegression
-
-if st.checkbox("select"):
-    theme.primaryColor
 
 st.title("Predict MLB Batting Average!⚾️")
 
-if st.checkbox('checkbox'):
-    st.image("mike.png")
-
-            
-import pandas as pd 
-df = pd.read_csv('savant_data (16).csv')
-
 df = df[df['total_pitches'] > 10]
 df = df.dropna()
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 x = df[['player_id','hits', 'total_pitches']]
 y = df['ba']
@@ -42,13 +34,6 @@ new_input = [[593423, 3 ,17]]
 new_output = model.predict(new_input)
 # summarize input and output
 print(new_input, new_output) 
-
-st.write(new_input)
-st.write(new_output)
-
-if st.checkbox("Show number of rows and columns"):
-        st.write(f'Rows: {df.shape[0]}')
-        st.write(f'Columns: {df.shape[1]}')
 
 import streamlit as st
 import pickle
@@ -71,4 +56,49 @@ def predict(playerid, hits, total_pitches):
        # total_pitches == 17
     prediction = model.predict(pd.DataFrame([[playerid, hits, total_pitches]], columns = ['playerid', 'hits', 'total_pitches']))
     return prediction
+
+col, col2, col3 = st.columns(3)
+
+st.header('Enter the characteristics: ')
+
+
+hitter = st.selectbox(
+    'Select a pitcher: ', ['593423', '541640'])
+if hitter == '593423':
+    st.write(f"You selected Frankie Montas")
+    with col:
+        st.header("Frankie Montas")
+        st.image(image3)
+    with col2:
+        st.header(" ")
+        st.write(" ")
+        st.image(image5)
+    with col3:
+        st.header("Bryce Harper")
+        st.image(image4)
+
+elif hitter == '541640':
+    st.write(f"You selected Erasmo Ramierz")
+    
+st.write("Hitter: Bryce Harper")
+
+
+#playerid = st.number_input('playerid', min_value=1, max_value=10000000, value=1)
+#if playerid == 593423:
+    #'You selected: Mike Trout'
+ 
+hits = st.number_input('hits:', min_value=1, max_value=50, value=1)
+total_pitches = st.number_input('total_pitches:', min_value=1, max_value=50, value=1)
+
+
+
+if st.button('Predict Batting Average'):
+    price = predict(hitter, hits, total_pitches)
+    st.write(f"Predicted Batting Average for Bryce Harper against {hitter}")
+    st.success( price)
+
+
+if st.checkbox('checkbox'):
+    st.image("mike.png")
+
 
