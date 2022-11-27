007bfc0c65be8a51407b37bbdb95326ae3cec75a
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-11-27 14:27:34 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -12,6 +12,7 @@ st.button('Streamlit Button', help = "Click me!")
 st.title("Predict MLB Batting Average!⚾️")
 
 df = pd.read_csv('savant_data (16).csv')
+st.write('df.head()')
 
 if st.checkbox("Show number of rows and columns"):
   st.write(f'Rows: {df.shape[0]}')
