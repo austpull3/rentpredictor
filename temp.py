bcdf3ac1181b6aecc5d99c20d953b8ab58737197
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-11-27 12:32:54 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -6,35 +6,12 @@ import time
 
 import numpy as np
 
-#conn = sqlite3.connect('rentpredictor')
 
 
 st.title("Predict MLB Batting Average!⚾️")
 
+if st.checkbox('checkbox'):
+    st.image("mike.png")
 
-from PIL import Image 
-image = Image.open('rentpredictor/mike.png')
-#image2 = Image.open('/Users/austinpullar/Desktop/julio.png')
-#image3 = Image.open('/Users/austinpullar/Desktop/Frankie.png')
-#image4 = Image.open('/Users/austinpullar/Desktop/Bryce.png')
-#image5 = Image.open('/Users/austinpullar/Desktop/vs.jpeg')
-
-
-
-
-import base64
-
-"""## Home Run Bryce Harper!!!🎆"""
-file_ = open("/Users/austinpullar/Desktop/giphy.gif", "rb")
-contents = file_.read()
-data_url = base64.b64encode(contents).decode("utf-8")
-file_.close()
-
-st.markdown(
-    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-    unsafe_allow_html=True,
-)
-            
-            
             
             
