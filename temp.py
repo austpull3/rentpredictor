f5a1975fc82d61598e81031208db62557d21b48f
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-11-27 13:50:03 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -9,7 +9,7 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 if st.checkbox("select"):
-    primaryColor = st.get_option("theme.primaryColor")
+    primaryColor = theme.primaryColor
 
 st.title("Predict MLB Batting Average!⚾️")
 
