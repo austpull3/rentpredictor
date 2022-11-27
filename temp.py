35bded96fff43dd120d447802e51c2324152c88c
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-11-27 14:55:09 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -25,7 +25,7 @@ if mike:
     st.image("mike.png")
 
 if st.checkbox('checkbox'):
-    st.image("/Images/julio.png") 
+    st.image("rentpredictor/Images/julio.png") 
 
 df = df[df['total_pitches'] > 10]
 df = df.dropna()
