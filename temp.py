070433f3e5c4224dcff4f978664c6d095f2828bf
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-11-27 13:16:33 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -7,7 +7,23 @@ import time
 import numpy as np
 
 from sklearn.linear_model import LinearRegression
+x = df[['player_id','hits', 'total_pitches']]
+y = df['ba']
+x = x.to_numpy()
+y = y.to_numpy()
 
+model = LinearRegression()
+model.fit(x, y)
+y_pred = model.predict(x)
+for i in range(10):
+    print(x[i], y_pred[i])
+    
+# define input
+new_input = [[593423, 3 ,17]]
+# get prediction for new input
+new_output = model.predict(new_input)
+# summarize input and output
+print(new_input, new_output) 
 
 
 st.title("Predict MLB Batting Average!⚾️")
